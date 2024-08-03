242c85622fb402581e4f33f702f4a5b129fcee92
Mochamad Farhan Fitrahtur Rachmad <farhanfitrahtur@gmail.com>
2024-08-03 23:15:53 +0700

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+export default function Layout({ children }){
+    return(
+        <html lang="en">
+            <head>
+                <title>Learn next.js fundamental</title>
+            </head>
+            <body>
+                {children}
+            </body>
+        </html>
+    )
+}
